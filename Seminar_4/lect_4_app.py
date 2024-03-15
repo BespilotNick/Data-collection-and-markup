d494c062429de9dc59f08039d19aceb8afa76718
NickAllenov <AllenovNS@gmail.com>
2024-03-15 16:13:27 +0300

    xpath

--- Seminar_4/lect_4_app.py
+++ Seminar_4/lect_4_app.py
@@ -6,11 +6,25 @@ tree = etree.parse("Seminar_4/src/web_page.html")
 # title_element = tree.find("body/p")
 # print(title_element.text)
 
-list_item = tree.findall("body/ul/li")
-for li in list_item:
-    a = li.find('a')
+# list_item = tree.findall("body/ul/li")
+# for li in list_item:
+#     a = li.find('a')
 
-    if a is not None:
-        print(f'{li.text.strip()} {a.text}')
-    else:
-        print(li.text)
+#     if a is not None:
+#         print(f'{li.text.strip()} {a.text}')
+#     else:
+#         print(li.text)
+
+
+# title_element = tree.xpath('//title/text()')[0]
+# # print(title_element)
+
+# list_items = tree.xpath('//li')
+# for li in list_items:
+#     text = ''.join(map(str.strip, li.xpath('.//text()')))
+#     print(text)
+
+list_items = tree.xpath('//ul/descendant::li')
+for li in list_items:
+    text = ''.join(map(str.strip, li.xpath('.//text()')))
+    print(text)
