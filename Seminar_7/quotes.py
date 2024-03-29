db437ff22a996c5b73698f2b94f6ffb443009499
NickAllenov <AllenovNS@gmail.com>
2024-03-29 22:21:41 +0300

    quotes

--- Seminar_7/quotes.py
+++ Seminar_7/quotes.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
+import csv
 
 driver = webdriver.Chrome()
 driver.get("https://quotes.toscrape.com")
@@ -8,9 +9,24 @@ driver.get("https://quotes.toscrape.com")
 quotes = []
 
 while True:
-    quote_elements = driver.find_element(By.XPATH, '//div[@class="quote"]')
+    quote_elements = driver.find_elements(By.XPATH, '//div[@class="quote"]')
 
     for quote_element in quote_elements:
-        quote = quote_element.find_element(By.XPATH, '//span[@class="text"]')
-        author = 
+        quote = quote_element.find_element(By.XPATH, './/span[@class="text"]').text
+        author = quote_element.find_element(By.XPATH, './/span/small[@class="author"]').text
+        quotes.append({"quote": quote, "author": author})
 
+    next_button = driver.find_elements(By.XPATH, '//li[@class="next"]/a')
+    if not next_button:
+        break
+
+    next_button[0].click()
+
+    time.sleep(1)
+
+with open("quotes.csv", "w", encoding="utf-8", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=["quote", "author"])
+    writer.writeheader()
+    writer.writerows(quotes)
+
+driver.close()
