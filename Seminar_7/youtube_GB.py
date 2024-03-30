560044eac8f1a9cccdb913ae1463a5b338efc927
NickAllenov <AllenovNS@gmail.com>
2024-03-30 15:02:54 +0300

    selenium_GB_youtube

--- Seminar_7/youtube_GB.py
+++ Seminar_7/youtube_GB.py
@@ -12,23 +12,48 @@ url = 'https://www.youtube.com/@progliveru/videos'
 chrome_options = Options()
 chrome_options.add_argument(f'user-agent={USER_AGENT}')
 browser = webdriver.Chrome()
-browser.get(url)
+try:
+    browser.get(url)
 
-WebDriverWait(browser, 10).until(ec.presence_of_all_elements_located((By.TAG_NAME, 'body')))
+    WebDriverWait(browser, 10).until(ec.presence_of_all_elements_located((By.TAG_NAME, 'body')))
 
-page_height = browser.execute_script("return document.documentElement.scrollHeight")
-print(f'{page_height = }')
+    page_height = browser.execute_script("return document.documentElement.scrollHeight")
+    print(f'{page_height = }')
 
-pause_time = 3
-time.sleep(pause_time)
-
-last_height = page_height
-while True:
-    browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+    pause_time = 3
     time.sleep(pause_time)
-    new_page_height = page_height = browser.execute_script("return document.documentElement.scrollHeight")
-    if new_page_height == last_height:
-        break
-    last_height = new_page_height
-print(f'{last_height = }')
 
+    last_height = page_height
+    while True:
+        browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+        time.sleep(pause_time)
+        new_page_height = browser.execute_script("return document.documentElement.scrollHeight")
+        if new_page_height == last_height:
+            break
+        last_height = new_page_height
+    print(f'{last_height = }')
+
+    video_titles_xpath = "//*[@id='video-title-link']"
+    metadata_views_xpath = "//div[@id='metadata-line']/span[1]"
+    metadata_published_xpath = "//div[@id='metadata-line']/span[2]"
+
+    video_titles = browser.find_elements(By.XPATH, video_titles_xpath)
+    metadata_views = browser.find_elements(By.XPATH, metadata_views_xpath)
+    metadata_published = browser.find_elements(By.XPATH, metadata_published_xpath)
+
+
+    video_data = {}
+
+    for i in range(len(video_titles)):
+        title = video_titles[i].text
+        views = metadata_views[i].text
+        published = metadata_published[i].text
+
+        video_data[title] = {"views": views, "published": published}
+
+        with open('GB_youtube.json', 'w', encoding='utf-8') as file:
+            json.dump(video_data, file, ensure_ascii=False, indent=4)
+except Exception as E:
+    print(f'Произошла ошибка, {E}')
+finally:
+    browser.quit()
